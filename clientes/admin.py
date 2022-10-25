cb5681e47b60e1ace9d1378c2491420641348916
mordren <joaokko@gmail.com>
2022-10-25 13:40:26 -0300

    Exemplo Relacionamento OneToMany

--- clientes/admin.py
+++ clientes/admin.py
@@ -1,7 +1,8 @@
 from telnetlib import DO
 from django.contrib import admin
-from .models import Person, Documento
+from .models import Person, Documento, Venda
 # Register your models here.
 
 admin.site.register(Person)
 admin.site.register(Documento)
+admin.site.register(Venda)
--- clientes/models.py
+++ clientes/models.py
@@ -14,8 +14,21 @@ class Person(models.Model):
     salary = models.DecimalField(max_digits=8, decimal_places=2)
     bio = models.TextField()
     photo = models.ImageField(upload_to='clients_photos', null=True, blank=True)
+    #relação de um para um quando cada um objeto é ligado ao outro e não poderá ser ligado para outro.    
     doc = models.OneToOneField(Documento, null=True, blank=True, on_delete=models.CASCADE)       
 
 #o que vai retornar
     def __str__(self):
         return self.first_name+' '+self.last_name
+    
+    
+class Venda(models.Model):
+    numero = models.CharField(max_length=30)
+    valor = models.DecimalField(max_digits=8, decimal_places=2)
+    impostos = models.DecimalField(max_digits=8, decimal_places=2)
+    desconto = models.DecimalField(max_digits=8, decimal_places=2)
+    #Aqui não vai poder deletar a pessoa sem deletar antes as vendas, está protegido.
+    pessoa = models.ForeignKey(Person, null=True, blank=True, on_delete=models.PROTECT)
+    
+    def __str__(self):
+        return self.numero
