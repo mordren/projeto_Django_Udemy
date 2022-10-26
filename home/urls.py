8a239c1be7f5e59bf604ab6d95a4f17c85c1ed39
mordren <joaokko@gmail.com>
2022-10-26 09:11:29 -0300

    Template View Home

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.views.generic.base import TemplateView
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('logout/', views.my_logout, name='logout'),
+    path('home2', TemplateView.as_view(template_name='home/home2.html'), name='home3'),
+    path('home3', views.HomePageView.as_view(), name='home3'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,15 +1,28 @@
+from cgi import test
 from telnetlib import LOGOUT
 from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 from django.shortcuts import redirect, render
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
+
+
+class HomePageView(TemplateView):
+    #necessário trocar o template_name para funcionar
+    template_name = "home/home3.html"
+    def get_context_data(self, **kwargs):
+        #recebe o context de herança da classe pai
+        context = super().get_context_data(**kwargs)
+        
+        #envio ao template através do context alguma informação.        
+        context['test'] = 'asdrubal'
+        return context
 
 # Create your views here.
 @login_required(login_url='../../login/')
 def home(request):         
     #import pdb; pdb.set_trace()
     return render(request, 'home/home.html')
-    
 
 def my_logout(request):
     logout(request)
